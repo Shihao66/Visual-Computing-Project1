b85488798a775df8274005cb1cfddc3866fcdfef
will <you@example.com>
2014-09-21 22:41:33 -0400

    remove src

--- src/test/java/csc301/exercise1/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package csc301.exercise1.util;
-
-public class Constants {
-
-	public static final String TORONTO  = "Toronto";
-	public static final String OTTAWA   = "Ottawa";
-	public static final String MONTREAL = "Montreal";
-	
-}
